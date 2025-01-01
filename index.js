e334c32bc6ad31d2cb2b70d18c8a36651a7d57da
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2025-01-01 08:49:29 +0530

    update

--- index.js
+++ index.js
@@ -1,39 +1,64 @@
 #!/usr/bin/env node
 
-const processManager = require("./lib/pm.js");
-const log = require("./lib/logger.js");
+const cluster = require('cluster');
+const os = require('os');
+const processManager = require('./lib/pm');
+const log = require('./lib/logger');
 
+const numCPUs = os.cpus().length;
 const args = process.argv.slice(2);
 const command = args[0];
+const processName = args[3] || 'NodeShield';
 
-switch (command) {
-  case 'start':
-    const script = args[1];
-    const name = args[3] || 'NodeShield';
-    const env = args[5] || 'development';
-    const cluster = args.includes('--cluster');
-    processManager.start(script, name, env, cluster);
-    break;
+log.printBanner();
+log.info("NodeShield Is Now Ready To Manage Your App!");
+         
+if (cluster.isMaster) {
+  log.info(processName, `Master process started on ${numCPUs} CPUs`);
 
-  case 'stop':
-    const stopName = args[1];
-    processManager.stop(stopName);
-    break;
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-  case 'restart':
-    const restartName = args[1];
-    processManager.restart(restartName);
-    break;
+  cluster.on('exit', (worker, code, signal) => {
+    log.error(processName, `Worker ${worker.process.pid} died. Forking a new worker.`);
+    cluster.fork();
+  });
 
-  case 'info':
-    const infoName = args[1];
-    processManager.info(infoName);
-    break;
+} else {
+  switch (command) {
+    case 'start':
+      const script = args[1];
+      const env = args[5] || 'development';
+      const clusterOption = args.includes('--cluster');
+      log.info(processName, `Starting process in ${env} mode...`);
+      processManager.start(script, processName, env, clusterOption);
+      break;
 
-  case 'list':
-    processManager.list();
-    break;
+    case 'stop':
+      const stopName = args[1];
+      log.info(processName, `Stopping process ${stopName}...`);
+      processManager.stop(stopName);
+      break;
 
-  default:
-    log.error('Command not found! Use start, stop, restart, info, or list.');
+    case 'restart':
+      const restartName = args[1];
+      log.info(processName, `Restarting process ${restartName}...`);
+      processManager.restart(restartName);
+      break;
+
+    case 'info':
+      const infoName = args[1];
+      log.info(processName, `Fetching information for process ${infoName}...`);
+      processManager.info(infoName);
+      break;
+
+    case 'list':
+      log.info(processName, 'Listing all running processes...');
+      processManager.list();
+      break;
+
+    default:
+      log.error(processName, 'Command not found! Use start, stop, restart, info, or list.');
+  }
 }
